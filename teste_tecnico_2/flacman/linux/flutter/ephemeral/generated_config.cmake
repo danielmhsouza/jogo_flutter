4f576eea96aba7999a841e16e22721cda97d45cc
Daniel Miguel <103042532+danielmhsouza@users.noreply.github.com>
2022-08-21 23:26:18 -0300

    test-2

---
+++ teste_tecnico_2/flacman/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/daniel/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/daniel/StudioProjects/flacman" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/daniel/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/daniel/StudioProjects/flacman"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/daniel/StudioProjects/flacman/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/daniel/StudioProjects/flacman/lib/main.dart"
+)
